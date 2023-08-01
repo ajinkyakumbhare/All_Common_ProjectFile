fc8503bbac976728c141708076916d82a8cce6b2
ajinkya <ashu.ajinkya29@gmail.com>
2023-08-01 15:20:41 +0530

    ProjectLightning

--- Script.js
+++ Script.js
@@ -1,3 +1,13 @@
-var course = " Ashu is Herofgrgerr"
-
+var course = "Zero to hero"
 console.log(course)
+
+function abc(){
+    var anothercourse = "Hero in ashu"
+    console.log(anothercourse)
+}
+abc()
+
+if(2==2){
+    var x=10
+    console.log(x)
+}
